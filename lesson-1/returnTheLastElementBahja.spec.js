5f7929248914e6203af7c83edf40c712891df770
bahja <baden021@student.wethinkcode.co.za>
2022-06-30 21:51:24 +0200

    Added test cases for return last element task
    Signed-off-by: bahja <baden021@student.wethinkcode.co.za>

--- lesson-1/returnTheLastElementBahja.spec.js
+++ lesson-1/returnTheLastElementBahja.spec.js
@@ -5,6 +5,10 @@ describe((`return the last element in an array`), function () {
         expect(returnTheLastElementBahja([1, 2, 3])).toBe(3)
     });
 
+    it('should return the only number is the array if an array with a single value is given', function (){
+        expect(returnTheLastElementBahja([1])).toBe(1)
+    });
+
     it('should return the last character in an array of characters', function () {
         expect(returnTheLastElementBahja(['x','y','z'])).toBe('z')
     });
@@ -15,6 +19,14 @@ describe((`return the last element in an array`), function () {
 
     it('should return an error message if null is given in place of an array', function () {
         expect(returnTheLastElementBahja(null)).toBe('Please enter a valid input')
+    });
+
+    it('should return an error message if argument not in an array is given', function () {
+        expect(returnTheLastElementBahja(2)).toBe('Please enter a valid input')
+    });
+
+    it('should return an error message if no argument is given', function () {
+        expect(returnTheLastElementBahja()).toBe('Please enter a valid input')
     });
 
 
